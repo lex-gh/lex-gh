43be51d3e3eb75d02ad866a0a3c250e1e9550dd1
abra-93 <maskall93@gmail.com>
2023-09-17 19:10:03 +0300

    add portfolio project

--- src/js/translate.js
+++ src/js/translate.js
@@ -14,7 +14,8 @@ export const i18n = {
     email: "Почта",
     social: "соц. сети",
     // Skills section
-    "skills desc": "Постоянно изучаю и практикую все нововедения в области Frontend-разработки.",
+    "skills desc":
+      "Постоянно изучаю и практикую все нововедения в области Frontend-разработки.",
     frontend: "Frontend разработка",
     backend: "Backend разработка",
     design: "Дизайн",
@@ -23,33 +24,45 @@ export const i18n = {
     "experience desc": "Хочу работать в хорошей команде над крутыми проектами.",
     "job title": "Младший Веб-разработчик",
     "job date": "Май, 2022 — Сентябрь, 2022",
-    "job desc": "Создавал landing page. Вносил правки в веб сайты исправлял ошибки.Учавствовал в создании компонентов на Vue.js",
+    "job desc":
+      "Создавал landing page. Вносил правки в веб сайты исправлял ошибки.Учавствовал в создании компонентов на Vue.js",
     "new job": "Младший Фронтенд разработчик",
     "new company": "Ваша компания",
-    "new desc": "У меня нет большого опыта коммерческой разработки, но есть большое желание получить его в вашей компании.",
+    "new desc":
+      "У меня нет большого опыта коммерческой разработки, но есть большое желание получить его в вашей компании.",
     now: "Сейчас",
     // education
     "education desc": " Cтараюсь узнавать что-то новое каждый день.",
     course_one_date: "Декабрь, 2021 — Декабрь, 2021",
-    course_one_desc: "Работа с основами HTML и CSS. Изучение основных концепций языка программирования JavaScript",
+    course_one_desc:
+      "Работа с основами HTML и CSS. Изучение основных концепций языка программирования JavaScript",
     course_two_date: "Декабрь, 2021 — Март, 2022",
-    course_two_desc: "Изучение HTML, CSS. Практическая разработка веб-сайта. Глубокое изучение JavaScript (функции, объекты, массивы, алгоритмы и структуры данных)",
+    course_two_desc:
+      "Изучение HTML, CSS. Практическая разработка веб-сайта. Глубокое изучение JavaScript (функции, объекты, массивы, алгоритмы и структуры данных)",
     course_three_date: "Март, 2022 — Сентябрь, 2022",
-    course_three_desc: "Изучение JavaScript, Решения алгоритмических задач. Изучение основ NodeJS. ООП, асинхронное программирование. Изучение TypeScript",
+    course_three_desc:
+      "Изучение JavaScript, Решения алгоритмических задач. Изучение основ NodeJS. ООП, асинхронное программирование. Изучение TypeScript",
     course_four_name: "Современный учебник JavaScript",
     course_four_date: "Постоянно",
     course_four_desc: "Использую учебник как шпаргалку.",
     // Portfolio
     "portfolio desc": "Постоянно практикую поученные знания.",
     link: "Посетить вебсайт",
-    project_one_desc: "Лендинг  психологов. Верстка с использованием Gulp, препроцессора SCSS и методологии БЭМ",
-    project_two_desc: "Целевая страница фотографа. Pixel perfect верстка с использованием препроцессора SCSS и методологии BEM. Возможность смены языка и цветов страницы",
-    project_three_desc: "Сайт поиска фильмов. Адаптивная верстка с использованием препроцессора SCSS и методологии БЭМ. Получение информации о фильмах с помощью API.",
-    project_four_desc: "Cайт погоды. Написан с использованием Vue.js и TailwindCSS. Получение информации через API. Показывает информацию о погоде для выбранного города.",
+    project_one_desc:
+      "Лендинг  психологов. Верстка с использованием Gulp, препроцессора SCSS и методологии БЭМ",
+    project_two_desc:
+      "Целевая страница фотографа. Pixel perfect верстка с использованием препроцессора SCSS и методологии BEM. Возможность смены языка и цветов страницы",
+    project_three_desc:
+      "Сайт поиска фильмов. Адаптивная верстка с использованием препроцессора SCSS и методологии БЭМ. Получение информации о фильмах с помощью API.",
+    project_four_desc:
+      "Cайт погоды. Написан с использованием Vue.js и TailwindCSS. Получение информации через API. Показывает информацию о погоде для выбранного города.",
     project_five_desc:
       "Таск-менеджер реализован на Vue3 (composition API) c использованием Vuex. Для взаимодействия с досками и задачами используется библиотека Vue Smooth DnD. Для хранения используется LocaleStorage.",
-    project_six_desc: "Игра змейка, написана на JavaScript. Реализован в виде пользовательского веб-компонента. Управление с помощью клавиатуры или игровых кнопок",
-    project_seven_desc: "Сайт вопросов и ответов на Vue + Pinia. Сервер на Node.js с использованием фреймворка Express. База данных MongoDB",
+    project_six_desc:
+      "Игра змейка, написана на JavaScript. Реализован в виде пользовательского веб-компонента. Управление с помощью клавиатуры или игровых кнопок",
+    project_seven_desc: "Веб приложение по поиску ментора.",
+    project_eight_desc:
+      "Сайт вопросов и ответов на Vue + Pinia. Сервер на Node.js с использованием фреймворка Express. База данных MongoDB",
     // Contact
     name_placeholder: "Имя",
     email_placeholder: "Почта",
@@ -71,7 +84,8 @@ export const i18n = {
     email: "email",
     social: "social",
     // Skills section
-    "skills desc": "I constantly study and practice all the innovations in the field of Frontend development.",
+    "skills desc":
+      "I constantly study and practice all the innovations in the field of Frontend development.",
     frontend: "Frontend Development",
     backend: "Backend Development",
     design: "UIUX Design",
@@ -80,33 +94,45 @@ export const i18n = {
     "experience desc": "I want to work in a good team on cool projects.",
     "job title": "Junior Web Developer",
     "job date": "May, 2022 — September, 2022",
-    "job desc": "Created a landing page. Edited websites and corrected errors. Participated in the creation of components on Vue.js",
+    "job desc":
+      "Created a landing page. Edited websites and corrected errors. Participated in the creation of components on Vue.js",
     "new job": "Junion Frontend Developer",
     "new company": "Your company",
-    "new desc": "I do not have much commercial development experience, but I have a great desire to get it in your company.",
+    "new desc":
+      "I do not have much commercial development experience, but I have a great desire to get it in your company.",
     now: "now",
     // education
     "education desc": "I try to learn something new every day.",
     course_one_date: "December, 2021 — December, 2021",
-    course_one_date: "Working with the basics of HTML and CSS. Learning the basic concepts of the JavaScript programming language",
+    course_one_date:
+      "Working with the basics of HTML and CSS. Learning the basic concepts of the JavaScript programming language",
     course_two_date: "December, 2021 — March, 2022",
-    course_two_desc: "Learning HTML, CSS. Practical web site development. JavaScript Deep Learning (Functions, Objects, Arrays, Algorithms and Data Structures)",
+    course_two_desc:
+      "Learning HTML, CSS. Practical web site development. JavaScript Deep Learning (Functions, Objects, Arrays, Algorithms and Data Structures)",
     course_three_date: "March, 2022 — September, 2022",
-    course_three_desc: "Learning JavaScript, Solving algorithmic problems. Learning the basics of NodeJS. OOP, asynchronous programming. Learning TypeScript",
+    course_three_desc:
+      "Learning JavaScript, Solving algorithmic problems. Learning the basics of NodeJS. OOP, asynchronous programming. Learning TypeScript",
     course_four_name: "The Modern JavaScript Tutorial",
     course_four_date: "constantly",
     course_four_desc: "I use the textbook as a cheat sheet.",
     // Portfolio
     "portfolio desc": "Constantly practicing what I have learned",
     link: "Visit website",
-    project_one_desc: "Landing page for psychologists. Layout using Gulp, SCSS preprocessor and BEM methodology",
-    project_two_desc: "Photographer landing page. Pixel perfect layout using SCSS preprocessor and BEM methodology. Ability to change the language and colors of the page",
-    project_three_desc: "Movie search site. Responsive layout using SCSS preprocessor and BEM methodology. Getting information about movies using the API.",
-    project_four_desc: "Weather website. Written using Vue.js and TailwindCSS. Getting information via API. Shows weather information for the selected city.",
+    project_one_desc:
+      "Landing page for psychologists. Layout using Gulp, SCSS preprocessor and BEM methodology",
+    project_two_desc:
+      "Photographer landing page. Pixel perfect layout using SCSS preprocessor and BEM methodology. Ability to change the language and colors of the page",
+    project_three_desc:
+      "Movie search site. Responsive layout using SCSS preprocessor and BEM methodology. Getting information about movies using the API.",
+    project_four_desc:
+      "Weather website. Written using Vue.js and TailwindCSS. Getting information via API. Shows weather information for the selected city.",
     project_five_desc:
       "The task manager is implemented on Vue3 (composition API) using Vuex. To interact with boards and tasks, the Vue Smooth DnD library is used. For storage, LocaleStorage is used.",
-    project_six_desc: "The snake game is written in JavaScript. Implemented as a custom web component. Control via keyboard or game buttons.",
-    project_seven_desc: "Q&A site on Vue + Pinia. Server on Node.js using the Express framework. MongoDB database",
+    project_six_desc:
+      "The snake game is written in JavaScript. Implemented as a custom web component. Control via keyboard or game buttons.",
+    project_seven_desc: "Web application for finding a mentor.",
+    project_eight_desc:
+      "Q&A site on Vue + Pinia. Server on Node.js using the Express framework. MongoDB database",
     // Contact
     name_placeholder: "Name",
     email_placeholder: "E-mail",
